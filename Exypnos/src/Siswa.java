31feb1774aa497adc20babf7ec4a0e70ee8d86c4
alleFarell <46789673+alleFarell@users.noreply.github.com>
2019-11-07 17:49:25 +0700

    101_Exypnos
    Pembuatan awal kelas dan lain-lain

---
+++ Exypnos/src/Siswa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Siswa extends Orang{
+    private String idSiswa;
+    private String namaSiswa;
+    private String jurusan;
+    private int tahunMasuk;
+
+    public Siswa(String idSiswa, String namaSiswa, String jurusan, int tahunMasuk) {
+        setIdSiswa(idSiswa);
+        setNamaSiswa(namaSiswa);
+        setJurusan(jurusan);
+        setTahunMasuk(tahunMasuk);
+    }
+
+    public String getIdSiswa() {
+        return idSiswa;
+    }
+
+    public void setIdSiswa(String idSiswa) {
+        this.idSiswa = idSiswa;
+    }
+
+    public String getNamaSiswa() {
+        return namaSiswa;
+    }
+
+    public void setNamaSiswa(String namaSiswa) {
+        this.namaSiswa = namaSiswa;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public int getTahunMasuk() {
+        return tahunMasuk;
+    }
+
+    public void setTahunMasuk(int tahunMasuk) {
+        this.tahunMasuk = tahunMasuk;
+    }
+    
+    
+    
+}
---
+++ Exypnos/src/Tentor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.*;
+public class Tentor extends Orang{
+    private List<Kelas> listKelas;
+    private String idTentor;
+    private String namaTentor;
+    private
+    
+}
